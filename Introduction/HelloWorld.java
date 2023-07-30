8cf3c2b35844b06a18794d321929d5408a9bf90f
Cihat Acar <cihat.acar4182@gmail.com>
2023-07-30 20:28:33 +0200

    Upload introduction codes.

---
+++ Introduction/HelloWorld.java
@@ -0,0 +1,9 @@
+package Introduction;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // This wil create a new line after printing the statement.
+        System.out.print("Hello "); // This will print the statement but will not go to the next line/
+        System.out.print("World!"); // So this line will be printing after Hello
+    }
+}
---
+++ Introduction/JavaReview.java
@@ -0,0 +1,8 @@
+package Introduction;
+
+public class JavaReview{
+    public static void main(String[] args){
+        // The main method executes the tasks of the class.
+        System.out.println("My first Java program from scratch!");
+    }
+}
